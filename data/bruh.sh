312a9e1b44e1c16e719f3b2d47449818da72cd2b
osh3276 <osh3276@gmail.com>
2026-05-26 14:48:31 -0400

    added temp maps

--- data/bruh.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e0e3318e6514938432554a9866c4bc689834b873f6c20ae518b60ef7330abcb1
-size 128
--- data/down.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ca58c42186935fa785ea19d0fd70696993403ffec5269c20a454e22412c378cf
-size 170
